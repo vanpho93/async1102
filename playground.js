433a2bcf42d14f211b91b641aae6c031736b5f10
Admin <vanpho02@gmail.com>
2017-03-25 14:03:57 +0700

    xong

--- playground.js
+++ playground.js
@@ -1,7 +1,4 @@
-// function add (a, b){
-//     return a + b;
-// }
-// const add = (a, b) => a + b;
+// (a + b) * h / 2
 
 const add = (a, b, cb) => {
     setTimeout(() => {
@@ -12,4 +9,25 @@ const add = (a, b, cb) => {
     }, 1000);
 };
 
+const mul = (a, b, cb) => {
+    setTimeout(() => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            return cb(new Error('Tham so phai co kieu number'));
+        }
+        cb(undefined, a * b);
+    }, 1000);
+};
 
+const div = (a, b, cb) => {
+    setTimeout(() => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            return cb(new Error('Tham so phai co kieu number'));
+        }
+        if (b === 0) return cb(new Error('Chia cho 0'));
+        cb(undefined, a / b);
+    }, 1000);
+};
+// add(4, 5, (err, result) => {
+//     if (err) return console.log(`${err} `);
+//     console.log(result);
+// });
