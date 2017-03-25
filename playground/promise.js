a5cc0867a1da21199de8e0335752fa7bf8cb9a8b
Admin <vanpho02@gmail.com>
2017-03-25 15:10:21 +0700

    xong

--- playground/promise.js
+++ playground/promise.js
@@ -6,16 +6,19 @@
 // .then(res => console.log('Thanh cong', res))
 // .catch(err => console.log('That bai'));
 
-const getPromise = (a, b) => (
+const add = (a, b) => (
     new Promise((resolve, reject) => {
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            return reject(new Error('Tham so phai co kieu number'));
-        }
-        resolve(a + b);
+        setTimeout(() => {
+            if (typeof a !== 'number' || typeof b !== 'number') {
+                return reject(new Error('Tham so phai co kieu number'));
+             }
+            resolve(a + b);
+        }, 1000);
     })
 );
 
-getPromise(4, 5)
-.then(res => console.log(res));
+add(4, 5)
+.then(res => console.log(res))
+.catch(err => console.log(err));
 
 
---
+++ playground/read.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+// fs.readFile('./a.txt', 'utf8', (err, data) => {
+//     if (err) return console.log(err);
+//     console.log(data);
+// });
+
+const read = (filename) => (
+    new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    })
+);
+
+read('./a.txt2')
+.then(res => console.log(res))
+.catch(err => console.log(err + ''));
