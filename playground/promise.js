2fced3384e6ae571736d3533a59f3d8978dbd72f
Apple <apple@Apples-MacBook-Pro.local>
2017-04-01 13:43:22 +0700

    keet thuc

--- playground/promise.js
+++ playground/promise.js
@@ -44,12 +44,27 @@ const div = (a, b) => (
 // .then(result => div(result, 2))
 // .then(square => console.log(square));
 
-const getSquare = (dai, rong, cao) => (
-    add(dai, rong)
-    .then(total => mul(total, cao))
-    .then(res => div(res, 2))
-);
+// const getSquare = (dai, rong, cao) => (
+//     add(dai, rong)
+//     .then(total => mul(total, cao))
+//     .then(res => div(res, 2))
+// );
 
-getSquare(4, '5', 6)
-.then(square => console.log(square))
-.catch(err => console.log(err + ''));
+// getSquare(4, '5', 6)
+// .then(square => console.log(square))
+// .catch(err => console.log(err + ''));
+
+const getSquare = async (a, b, h) => {
+    try {
+        const total = await add(a, b);
+        const abh = await mul(total, h);
+        const square = await div(abh, 2);
+        return Promise.resolve(square);
+    } catch (e) {
+        return Promise.reject(e);
+    }
+};
+
+getSquare(3, 4, '2')
+.then(square => console.log('Dien tich:', square))
+.catch(err => console.log('Loi:', err));
